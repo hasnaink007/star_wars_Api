d0a7bd9fad3629fb4b902200b0f5eaf6f3ca750b
Farooq CH <farooqch11@gmail.com>
2018-04-14 13:44:18 +0500

    set trusted proxy for heroku setup

--- config/env/production.js
+++ config/env/production.js
@@ -278,7 +278,7 @@ module.exports = {
     * (https://sailsjs.com/config/http)                                        *
     *                                                                          *
     ***************************************************************************/
-    // trustProxy: true,
+    trustProxy: true,
 
   },
 
