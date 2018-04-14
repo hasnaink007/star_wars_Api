c7c749b8333f96fd89e002c93ec68ed31956b739
Farooq CH <farooqch11@gmail.com>
2018-04-15 00:08:03 +0500

    added swapi package

--- api/controllers/PlanetController.js
+++ api/controllers/PlanetController.js
@@ -1,40 +1,45 @@
-/**
- * PlanetController
- *
- * @description :: Server-side actions for handling incoming requests.
- * @help        :: See https://sailsjs.com/docs/concepts/actions
- */
+const swapi = require('swapi-node');
 
 module.exports = {
 
-  create: function(req, res){
-    var params = {
-      name: req.body.planet.name
-    };
-    Planet.create(params).exec(function (err, planet) {
-      if(err)
-        res.serverError(err);
-      res.ok(planet);
+  create: function(req, res, next){
+    if (_.isUndefined(req.body.name))
+      return res.badRequest('Name is required.');
+    if (!_.isString( req.body.name) )
+      return res.badRequest("Name must be string.");
+    var params = {name: req.body.name};
+    Planet.findOne(params).exec(function (err, planet) {
+      if (err) return next(err);
+      if(planet === undefined){
+        swapi.get("http://swapi.co/api/planets/?search="+ req.body.name).then(function(result) {
+          console.log(result);
+          if(result.count == 0 )return res.badRequest(["Invalid Planet Name."]);
+          params.swapi = result.results[0];
+          Planet.create(params).exec(function (err, newPlanet) {
+            if(err)
+              res.serverError(err);
+            res.created(newPlanet);
+          });
+        });
+      }else
+        res.created(planet);
     });
   },
 
-  list: function(req, res){
+  list: function(req, res, nex){
     Planet.find({}).exec(function (err, planets) {
-      if(err)
-        res.serverError(err);
+      if (err) return next(err);
       res.ok(planets);
     })
   },
 
-  show: function(req, res){
+  show: function(req, res, next){
     var id = req.param('id');
-    Planet.findOne({id: id}).populate('comments').exec(function(err,data){
-      if(err)
-        res.serverError(err);
+    Planet.findOne({id: id}, function(err,data){
+      if(data === undefined) return res.notFound("Planet Not Found.");
+      if (err) return next(err);
       res.ok(data);
     });
   }
-
-
 };
 
--- api/models/Planet.js
+++ api/models/Planet.js
@@ -1,27 +1,32 @@
-/**
- * Planet.js
- *
- * @description :: A model definition.  Represents a database table/collection/etc.
- * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
- */
-
 module.exports = {
-
   attributes: {
     name: {
       type: 'string',
       required: true,
       maxLength: 128
     },
-    // lastName: {
-    //   type: 'string',
-    //   size: 128
-    // },
+    swapi_id:{
+      type: 'number',
+      required: true
+    },
+    swapi: {
+      type: 'json'
+    },
     comments: {
       collection: 'Comment',
       via: 'planet'
     }
   },
-
+  beforeCreate: function (values, cb) {
+    var url = values.swapi.url;
+    values.swapi_id = url.match(/\d+/g).map(Number)[0]
+    cb();
+  },
+  validationMessages: {
+    name: {
+      required: 'Name is required',
+      maxLength: 'The name that was provided was too long.'
+    }
+  }
 };
 
